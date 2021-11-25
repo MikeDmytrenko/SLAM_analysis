62a5e2be1feecc6f9f54bf66fbe696f9d5429913
Mike <myke.dmytrenko@gmail.com>
2021-11-25 18:30:37 +0100

    first

--- src/read_arr.py
+++ src/read_arr.py
@@ -4,10 +4,13 @@ import numpy as np
 
 
 array1=np.load("array.npy")
+array1=array1
+array1[:,2]=array1[:,2]-array1[0][2]
+
 
 array2=np.load("array1.npy")  
 smaller_array=[]
-bigger_array=[]
+bigger_array=[[]]
 
 
 if len(array1)<=len(array2):
@@ -20,7 +23,7 @@ else:
 smallest_time=100000.0
 bigger_id=[]
 diff_arr=[]
-dist=0.0
+
 for eachS in smaller_array:
     for eachB in bigger_array:
         time_diff=abs(eachS[2]-eachB[2])
@@ -28,7 +31,7 @@ for eachS in smaller_array:
             smallest_time=time_diff
             bigger_id=eachB
 
-    diff_arr.append(math.hypot(eachS[0]-bigger_id[0],eachS[1]-bigger_id[1]))
+    diff_arr.append([math.hypot(eachS[0]-bigger_id[0],eachS[1]-bigger_id[1]),round(eachS[2]-smaller_array[0][2],3)])
 
 
 print("done")
